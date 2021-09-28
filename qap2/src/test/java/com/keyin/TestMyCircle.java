1ec7c22374cebbed103fbdbe63ac92902d209e06
CameronBrenton <Cameron.Brenton@keyin.com>
2021-09-28 16:48:56 -0230

    First commit

---
+++ qap2/src/test/java/com/keyin/TestMyCircle.java
@@ -0,0 +1,20 @@
+package com.keyin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public static void main(String[] args){
+        MyCircle circle1 = new MyCircle();
+		System.out.println(circle1);
+    }
+}
