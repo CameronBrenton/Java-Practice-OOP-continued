b32bd97a4da3b5ed99695489fb367c1f733048cf
CameronBrenton <Cameron.Brenton@keyin.com>
2021-09-28 19:50:40 -0230

    First commit

---
+++ Sept.28-QAP2/TestMyCircle.java
@@ -0,0 +1,11 @@
+import com.keyin.MyCircle;
+import com.keyin.MyPoint;
+
+public class TestMyCircle {
+	public static void main(String[] args){
+		MyCircle circle1 = new MyCircle();
+		System.out.println("Hello!");
+		MyPoint mypoint1 = new MyPoint(2,4);
+        	System.out.println(mypoint1);
+	}
+}
