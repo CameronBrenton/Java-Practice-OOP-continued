650d0333fb8d293125f2f41d7b3a97dfdf2b7016
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-01 13:35:32 -0230

    Fixed the convertTo() method; changed where some conversions where done.

--- Sept.28-QAP2/Problem2/Money.java
+++ Sept.28-QAP2/Problem2/Money.java
@@ -23,35 +23,35 @@ public class Money {
 		this.dollars -= money.dollars;
 		this.cents -= money.cents;
 	}
-
+	
 	public double compareTo(Money money){
 		double moneyDollars = (double) (Math.floor(money.dollars));
-		double moneyCents = (double) (Math.floor(money.cents) * .001);
+		double moneyCents = (double) (Math.floor(money.cents));
 		if (this.dollars == moneyDollars && this.cents == moneyCents){
 			return 0;
 		}
 		else if(this.dollars == moneyDollars){
 			if(this.cents > money.cents){
 				double difference;
-				difference = (double)(this.cents) - (double)(moneyCents);
+				difference = (this.cents * .01) - (moneyCents * .01);
 				return difference;
 			}
 			else{
 				double difference;
-				difference = (double)(moneyCents) - (double)(this.cents);
+				difference = (moneyCents * .01) - (this.cents * .01);
 				return difference;
 			}
 		}
 		else if(this.dollars > moneyDollars){
 			if(this.cents > moneyCents){
 				double difference;
-				difference = (double)(this.cents) - (double)(moneyCents);
+				difference = (this.cents * .01) - (moneyCents * .01);
 				difference += this.dollars - moneyDollars;
 				return difference;
 			}
 			else if(this.cents < moneyCents){
 				double difference;
-				difference = (double)(moneyCents) - (double)(this.cents);
+				difference = (moneyCents * .01) - (this.cents * .01);
 				difference += this.dollars - moneyDollars;
 				return difference;
 			}
@@ -64,13 +64,13 @@ public class Money {
 		else if(this.dollars < moneyDollars){
 			if(this.cents > moneyCents){
 				double difference;
-				difference = (double)(this.cents) - (double)(moneyCents);
+				difference = (this.cents * .01) - (moneyCents * .01);
 				difference += moneyDollars - this.dollars;
 				return difference;
 			}
 			else if(this.cents < moneyCents){
 				double difference;
-				difference = (double)(moneyCents) - (double)(this.cents);
+				difference = (moneyCents * .01) - (this.cents * .01);
 				difference += moneyDollars - this.dollars;
 				return difference;
 			}
