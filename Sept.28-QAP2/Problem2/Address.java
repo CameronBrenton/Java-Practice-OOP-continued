2ebd94f2ce668957fd86e80ef1452482c533e5ad
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-02 11:10:16 -0230

    Made class variables, an parameterized Address constructor, and a toString() method.

--- Sept.28-QAP2/Problem2/Address.java
+++ Sept.28-QAP2/Problem2/Address.java
@@ -1,5 +1,19 @@
 package Problem2;
 
 public class Address {
-	
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+
+	public Address(String street, String city, String state, String zip){
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+
+	toString(){
+		return "Address[" + street + ", " + city + ", " + state + ", " + zip;
+	}
 }
