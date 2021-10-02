710c6ee82e067159c4629f17bd513b2cfdad8af4
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-02 19:13:02 -0230

    Fixed the datatype for the class variable and class constructor 'home'.

--- Sept.28-QAP2/Problem2/Person.java
+++ Sept.28-QAP2/Problem2/Person.java
@@ -3,15 +3,15 @@ package Problem2;
 public class Person {
 	private String lastName;
 	private String firstName;
-	private String home;
+	private Address home;
 
-	public Person(String lastName, String firstName, String home){
+	public Person(String lastName, String firstName, Address home){
 		this.lastName = lastName;
 		this.firstName = firstName;
 		this.home = home;
 	}
 
-	public toString(){
+	public String toString(){
 		return "Person[" + lastName + ", " + firstName + ", " + home + "]";
 	}
 }
