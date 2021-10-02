9da3148202527bbc2936ebc730e8ab2d722690c5
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-02 12:16:13 -0230

    Made the Person constructor, and the toString() method

--- Sept.28-QAP2/Problem2/Person.java
+++ Sept.28-QAP2/Problem2/Person.java
@@ -1,5 +1,17 @@
 package Problem2;
 
 public class Person {
-	
+	private String lastName;
+	private String firstName;
+	private String home;
+
+	public Person(String lastName, String firstName, String home){
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.home = home;
+	}
+
+	public toString(){
+		return "Person[" + lastName + ", " + firstName + ", " + home + "]";
+	}
 }
