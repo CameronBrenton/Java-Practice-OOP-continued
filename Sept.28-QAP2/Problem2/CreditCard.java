154f8910fa64cfc0db7302421e3c91387160317a
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-03 13:02:10 -0230

    Added comments :). We want our readings to know what's going on :).

--- Sept.28-QAP2/Problem2/CreditCard.java
+++ Sept.28-QAP2/Problem2/CreditCard.java
@@ -1,10 +1,14 @@
+// This is the CreditCard class
+
 package Problem2;
 
 public class CreditCard {
+	//Class variables
 	private Person owner;
 	private Money balance;
 	private Money creditLimit;
 
+	//CreditCard parameterized constructor
 	public CreditCard(Person newCardHolder, Money limit){
 		balance = new Money(0);
 		creditLimit = new Money(0);
@@ -12,18 +16,24 @@ public class CreditCard {
 		owner = newCardHolder;
 	}
 
+	// gets the balance on the credit card
 	public Money getBalance(){
 		return balance;
 	}
 
+	// gets the credit limit on the credit card
 	public Money getCreditLimit(){
 		return creditLimit;
 	}
 
+	// toString() method 
 	public String getPersonals(){
 		return owner.toString();
 	}
 
+	// This method adds the charge to the card using the money class's add() method. 
+	// We use an if statement to check if the credit limit is exceeded and if it is, then reverse the charge using the money class's
+	// subtract() method. We then print a message notifiying the user that the charge was revered because the credit limit was exceeded.
 	public void charge(Money amount){
 		balance.add(amount);
 		if(balance.compareTo(creditLimit) == 1){
@@ -32,6 +42,7 @@ public class CreditCard {
 		}
 	}
 
+	// We can make a payment on our account by using the money class's subtract method() on the balance.
 	public void payment(Money amount){
 		balance.subtract(amount);
 	}
