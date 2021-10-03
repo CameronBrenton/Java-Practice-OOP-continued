53e7da30bf9ce4217b61c03ea4db6c470a5d3f90
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-03 13:05:51 -0230

    Added comments :)

--- Sept.28-QAP2/Problem2/Address.java
+++ Sept.28-QAP2/Problem2/Address.java
@@ -1,11 +1,15 @@
+// This is the Address class.
+
 package Problem2;
 
 public class Address {
+	// class variables
 	private String street;
 	private String city;
 	private String state;
 	private String zip;
 
+	//Address parameterized constructor
 	public Address(String street, String city, String state, String zip){
 		this.street = street;
 		this.city = city;
@@ -13,6 +17,7 @@ public class Address {
 		this.zip = zip;
 	}
 
+	// toString() method
 	public String toString(){
 		return "Address[" + street + ", " + city + ", " + state + ", " + zip + "]";
 	}
