9418d43f856a6a092239b2cb87ca3970c5c94906
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-03 12:40:04 -0230

    Added comments :).

--- Sept.28-QAP2/Problem2/Person.java
+++ Sept.28-QAP2/Problem2/Person.java
@@ -1,3 +1,5 @@
+// This is the Person class
+
 package Problem2;
 
 public class Person {
@@ -5,12 +7,14 @@ public class Person {
 	private String firstName;
 	private Address home;
 
+	// Person parameterized constructor
 	public Person(String lastName, String firstName, Address home){
 		this.lastName = lastName;
 		this.firstName = firstName;
 		this.home = home;
 	}
 
+	// toString method()
 	public String toString(){
 		return "Person[" + lastName + ", " + firstName + ", " + home + "]";
 	}
