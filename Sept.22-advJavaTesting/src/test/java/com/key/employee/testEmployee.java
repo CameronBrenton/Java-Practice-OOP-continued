072941a8d9059d38cda2d04e7680973a419287f3
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-03 23:15:17 -0230

    Implemented new methods to how the tests are performed.

--- Sept.22-advJavaTesting/src/test/java/com/key/employee/testEmployee.java
+++ Sept.22-advJavaTesting/src/test/java/com/key/employee/testEmployee.java
@@ -5,37 +5,58 @@ import org.junit.Test;
 
 public class testEmployee {
 
-    public static void main(String[] args){
+    @Test
+    public void testGetId() {
         Employee employee1 = new Employee(01, "John", "Patrick", 4500);
-        Employee employee2 = new Employee(employee1);
-
-        System.out.println(employee2.toString());
-        System.out.println("John's salary is: $" + employee2.getAnnualSalary());
-        System.out.println("Employee count: " + Employee.count);
-        Assert.assertFalse("The employee count should be 2", Employee.count != 2);
-        employee2.raiseSalary(20);
-        System.out.println("John's salary after 20% raise is: " + employee2.getAnnualSalary());
-
-        Assert.assertTrue(employee2.getAnnualSalary() == 64800);
-        Assert.assertFalse(employee2.getAnnualSalary() != 64800);
-
-        Employee employee3 = new Employee(02, "James", "Woodley", 5000);
-        Employee employee4 = new Employee(employee3);
-
-        System.out.println(employee4.toString());
-        System.out.println("Jame's salary is: $" + employee4.getAnnualSalary());
-        System.out.println("Employee count: " + Employee.count);
-        employee4.raiseSalary(20);
-        System.out.println("Jame's salary after 20% raise is: " + employee4.getAnnualSalary());
-
-        Assert.assertFalse("James salary after raise should be 72000", employee4.getAnnualSalary() != 72000);
-
-        System.out.println(employee4.getName());
-        Assert.assertTrue(employee4.getFirstName() == "James");
-        Assert.assertFalse(employee4.getFirstName() != "James");
-
-        Assert.assertTrue("The name does not match", employee4.getName() == "James Woodley");
-        Assert.assertFalse("The name does not match",employee4.getName() != "James Woodley");
-
+        Assert.assertTrue(employee1.getID() == 1);
     }
+
+
+    @Test
+    public void testGetFirstName(){
+        Employee employee2 = new Employee(02, "John", "Patrick", 4500);
+        Assert.assertTrue(employee2.getFirstName() == "John");
+    }
+
+    @Test
+    public void testGetLastName(){
+        Employee employee3 = new Employee(03, "Tracy", "Paige", 4750);
+        Assert.assertTrue(employee3.getLastName() == "Paige");
+    }
+
+    @Test
+    public void testGetName(){
+        Employee employee4 = new Employee(04, "Stan", "Nowlin", 4300);
+        Assert.assertTrue(employee4.getName() == "Stan Nowlin");
+    }
+
+    @Test
+    public void testGetSalary(){
+        Employee employee5 = new Employee(05, "Morgan", "Miller", 4500);
+        Assert.assertTrue(employee5.getSalary() == 4500);
+    }
+
+    @Test
+    public void testSetSalary(){
+        Employee employee6 = new Employee(06, "Richard", "Collins", 5000);
+        // Set the salary
+        employee6.setSalary(5000);
+        Assert.assertTrue(employee6.getSalary() == 5000);
+    }
+
+    @Test
+    public void getAnnualSalary(){
+        Employee employee7 = new Employee(07, "Mike", "Morand", 5100);
+        Assert.assertTrue(employee7.getAnnualSalary() == (12 * employee7.getSalary()));
+    }
+
+    @Test
+    public void raiseSalary(){
+        Employee employee8 = new Employee(8, "Lisa", "Boudreau", 5100);
+        // Raise the salary
+        employee8.raiseSalary(10);
+        Assert.assertTrue(employee8.getSalary() == (5100 * 1.10));
+    }
+
 }
+
