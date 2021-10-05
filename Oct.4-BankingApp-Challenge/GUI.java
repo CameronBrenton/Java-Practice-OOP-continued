b48637bd7197f4468469bd672469b6913e5431f8
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-05 10:39:40 -0230

    Added comments, changed some values for the button.

--- Oct.4-BankingApp-Challenge/GUI.java
+++ Oct.4-BankingApp-Challenge/GUI.java
@@ -3,17 +3,16 @@ import javax.swing.*;
 public class GUI {
 
 	public GUI(){
-	JFrame frame1 = new JFrame();
+	JFrame frame1 = new JFrame(); // Create frame object
 
-	JButton button1 = new JButton("Click here");
-	button1.setBounds(150, 100, 100, 50);
-
-	frame1.add(button1);
+	JButton button1 = new JButton("Click here"); // Create button object
+	button1.setBounds(140,25,75,75);
+	frame1.add(button1); // Add button to frame
 	
-	frame1.setSize(500, 500);
+	frame1.setSize(500, 500); // Frame Dimensions
 	frame1.setLayout(null);
-	frame1.setVisible(true);
-	
+	frame1.setVisible(true); // Make visible
+
 	}
 	
 }
