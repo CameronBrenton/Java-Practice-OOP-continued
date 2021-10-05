23c3b2b02d18cb9656e14138f1131e81f33d9578
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-05 19:07:34 -0230

    Filled out the CheckingAccount class, with all it's methods.

---
+++ Oct.4-BankingApp-Challenge/BankAccountClasses/CheckingAccount.java
@@ -0,0 +1,30 @@
+package BankAccountClasses;
+
+public class CheckingAccount {
+	private Person owner;
+	private Money balance;
+
+	public CheckingAccount(Person newAccountHolder, Money balance){
+		owner = newAccountHolder;
+		this.balance = balance;
+		
+	}
+
+	public Money getBalance(){
+		return balance;
+	}
+
+	public void transfer(Money amount){
+		balance.subtract(amount);
+		if(balance.compareTo(amount) == -1){
+			balance.add(amount);
+			System.out.println("The charge has been reversed. " +
+			"You do not have enough funds to make this transfer");
+		}
+	}
+
+	public String getPersonals(){
+		return owner.toString();
+	}
+
+}
