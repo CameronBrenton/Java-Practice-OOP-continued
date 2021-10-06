9e687a7393a6853ec08d432dcef86a96c813d095
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-06 13:40:58 -0230

    Filled out some code for the MainWindow class

---
+++ Oct.4-BankingApp-Challenge/Main/MainWindow.java
@@ -0,0 +1,103 @@
+package Main;
+import javax.swing.*;
+
+import BankAccountClasses.CheckingAccount;
+import BankAccountClasses.Address;
+import BankAccountClasses.Money;
+import BankAccountClasses.Person;
+import BankAccountClasses.SavingsAccount;
+import BankAccountClasses.UserAccount;
+import java.awt.event.*;
+
+public class MainWindow implements ActionListener{
+	private CheckingAccount checkingAccount;
+	private SavingsAccount savingsAccount;
+	public JFrame frame1 = new JFrame("YourBankingApp"); // Create our window frame and name it "YourBankingApp".
+	public JFrame logInFrame;
+	
+	
+
+	public MainWindow(){
+
+
+		JPanel panel1 = new JPanel(); // Create the panels
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		JPanel panel4 = new JPanel();
+		//panel1.add(textArea); // Add the textArea component to the panel component
+		JTabbedPane tabbedPane = new JTabbedPane(); // Create a tabbed pane object
+		tabbedPane.setBounds(0, 0, 375, 575); // Set the dimensions of the TabbedPane
+		tabbedPane.add("MyAccounts", panel1); // Add the panels to the Tabbed Pane
+		tabbedPane.add("Transfers", panel2);
+		tabbedPane.add("BillPayments", panel3);
+		tabbedPane.add("Profile", panel4);
+		frame1.add(tabbedPane); // Add the TabbedPane to to the frame.
+
+		String[] comboBoxAccountChoices = { // Create the dropdown menu options for MyAccounts pane
+			"Savings Account",
+			"Checking Account"
+		};
+		final JLabel label = new JLabel();
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setSize(300, 300);
+		JButton button2 = new JButton("Show Account");
+		button2.setBounds(200, 100, 100, 20);
+		final JComboBox comboBox = new JComboBox(comboBoxAccountChoices);
+		comboBox.setBounds(25, 50, 75, 25);
+		panel1.add(comboBox);
+		panel1.add(label);
+		panel1.add(button2);
+
+		final JLabel label2 = new JLabel();
+		label2.setHorizontalAlignment(JLabel.CENTER);
+		label2.setSize(300, 300);
+		JButton button3 = new JButton("Make Transfer");
+		button3.setBounds(200, 400, 100, 20);
+		final JComboBox comboBox2 = new JComboBox(comboBoxAccountChoices);
+		comboBox2.setBounds(25, 50, 75, 25);
+		panel2.add(comboBox2);
+		panel2.add(label2);
+		panel2.add(button3);
+		
+
+		frame1.setSize(375, 600); // Frame Dimensions to fit a large smartphone
+		frame1.setLayout(null);
+		frame1.setVisible(true); // Make the frame window visible
+		
+		
+		button2.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				String data = "Account Selected: " +
+				comboBox.getItemAt(comboBox.getSelectedIndex());
+				label.setText(data);
+			}
+		});
+		button3.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				String data = "           Transfer Successful!           ";
+				label2.setText(data);
+			}
+		});
+
+		
+
+		
+
+		//Person personCameron = new Person("Cameron ", "Brenton", new Address("122 Lloyd Street", "St.Johns", "Newfoundland & Labrador", "EEE 222"));
+
+		//UserAccount userCameron = new UserAccount(personCameron, "i01c", "Spaghetti");
+		//SavingsAccount savingsAccount = new SavingsAccount(personCameron, new Money(100000000));
+		//CheckingAccount checkingAccount = new CheckingAccount(personCameron, new Money(100000000));	
+
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO
+		
+		
+	}
+	
+}
