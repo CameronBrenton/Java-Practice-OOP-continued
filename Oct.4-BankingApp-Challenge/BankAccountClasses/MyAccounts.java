3358a5151438cf4ce6c8537fd5a7d2cbc6b1dee3
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-06 13:34:27 -0230

    Removed Savings and Checkings account abjects

--- Oct.4-BankingApp-Challenge/BankAccountClasses/MyAccounts.java
+++ Oct.4-BankingApp-Challenge/BankAccountClasses/MyAccounts.java
@@ -3,9 +3,8 @@ public class MyAccounts {
 	private SavingsAccount savingsAccount;
 	private CheckingAccount checkingAccount;
 
-	public MyAccounts(){
-		savingsAccount = new SavingsAccount(new Person("Cameron ", "Brenton", new Address("122 Lloyd Street", "St.Johns", "Newfoundland & Labrador", "EEE 222") ), new Money(100000000));
-		checkingAccount = new CheckingAccount(new Person("Cameron ", "Brenton", new Address("122 Lloyd Street", "St.Johns", "Newfoundland & Labrador", "EEE 222") ), new Money(100000000));
+	public MyAccounts(SavingsAccount newSavingsAccount, CheckingAccount newCheckingAccount){
 		
+
 	}
 }
