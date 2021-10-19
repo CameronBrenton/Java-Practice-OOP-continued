a5a050085f733e771ed7d244169a2dadfa634a5c
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-19 11:22:23 -0230

    Add instance variables, add class constructor

--- QAP3-Oct19/Problem1/Student.java
+++ QAP3-Oct19/Problem1/Student.java
@@ -3,5 +3,15 @@
 */
 
 public class Student extends Person{
+	protected String myIdNum;    // Student Id Number
+	protected double myGPA;      // grade point average
 	
+	public Student(String name, int age, String gender, String idNum, double gpa)  {
+		// use the super class’ constructor
+		super(name, age, gender);
+		// initialize what’s new to Student
+		myIdNum = idNum;
+		myGPA = gpa;
+	}
+
 }
