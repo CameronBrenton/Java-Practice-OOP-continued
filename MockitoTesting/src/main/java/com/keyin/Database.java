a6bd8213d73577dd63c00f5f16eb0eea2db70a1e
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-18 22:53:53 -0230

    First commit

---
+++ MockitoTesting/src/main/java/com/keyin/Database.java
@@ -0,0 +1,14 @@
+package com.keyin;
+
+public class Database {
+    public boolean isAvailable() {
+        // currently not implemented, as this is just demo used in a software test
+        return false;
+    }
+    public int getUniqueId() {
+        return 42;
+    }
+
+    public void setUniqueId(int eq) {
+    }
+}
---
+++ MockitoTesting/src/test/java/com/keyin/MockitoThrowsTest.java
@@ -0,0 +1,26 @@
+package com.keyin;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.Properties;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+
+public class MockitoThrowsTest {
+    // demonstrates the configuration of a throws with Mockito
+    // not a read test, just "testing" Mockito behavior
+    @Test
+    void testMockitoThrows() {
+        Properties properties = Mockito.mock(Properties.class);
+
+        when(properties.get(Mockito.anyString())).thenThrow(new IllegalArgumentException("Stuff"));
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> properties.get("A"));
+
+        assertEquals("Stuff", exception.getMessage());
+    }
+}
