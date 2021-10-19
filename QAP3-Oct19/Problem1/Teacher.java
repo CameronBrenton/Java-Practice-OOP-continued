0bab2ba5d46370b469727bafe6fe05655f37b21b
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-19 13:12:09 -0230

    Add the instance variables, the class constructor, getters and setters, toString.

--- QAP3-Oct19/Problem1/Teacher.java
+++ QAP3-Oct19/Problem1/Teacher.java
@@ -2,6 +2,42 @@
 * This is the Teacher subclass of Person.
 */
 
-public class Teacher extends Teacher{
+public class Teacher extends Person{
+	protected String subject; // Course subject the teacher is taking
+	protected double salary; // Teacher's annual salary
+
+	public Teacher(String myName, int myAge, String myGender, String subject, double salary){
+		// Access the parent class variable's
+		super(name, age, gender);
+		name = myName;
+		age = myAge;
+		gender = myGender;
+		this.subject = subject;
+		this.salary = salary;
+	}
+
+	// Getters
+	public String getSubject(){
+		return this.subject;
+	}
+
+	public double getSalary(){
+		return this.salary;
+	}
+
+	// Setters
+	public void setSubject(subject){
+		this.subject = subject;
+	}
+
+	public void setSalary(salary){
+		this.salary = salary;
+	}
+
+	// Describe itself
+	public String toString(){
+		return super.toString() + ", subject" + subject + ", salary" + salary;
+	}
+	
 	
 }
