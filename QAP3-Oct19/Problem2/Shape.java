08b6e4e06678d879a3650123859271e8443a982b
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-20 10:59:49 -0230

    First commit. Fill out the class with variables, setters, getters, boolean checks, and description method. Added comments.

---
+++ QAP3-Oct19/Problem2/Shape.java
@@ -0,0 +1,45 @@
+/*
+* This is the Shape superclass
+*/
+
+
+public class Shape {
+	protected String color; // colour of the shape
+	protected boolean filled; // Is the shape filled? "Y" or "N"
+
+	// Default class constructor
+	public Shape(){
+		color = "red";
+		filled = true;
+	}
+
+	// Parameterized class constructor
+	public Shape(String color, boolean filled){
+		this.color = color;
+		this.filled = filled;
+	}
+
+	// Getters
+	public String getColor(){
+		return this.color;
+	}
+
+	// Setters
+	public void setColor(String color){
+		this.color = color;
+	}
+
+	public void setFilled(boolean filled){
+		this.filled = filled;
+	}
+
+	// True/False
+	public boolean isFilled(){
+		return filled;
+	}
+
+	// Description
+	public String toString(){
+		return "This Shape has the color=" + color + ", filled=" + filled;
+	}
+}
