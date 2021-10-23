a67764df0523f28fc135f18c4a8371c96bb5e8d5
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-23 12:33:52 -0230

    Add toString() method, add comments.

--- QAP3-Oct19/Problem1/Student.java
+++ QAP3-Oct19/Problem1/Student.java
@@ -2,10 +2,12 @@
 * This is the Student subclass of Person.
 */
 
+
 public class Student extends Person{
 	protected String myIdNum;    // Student Id Number
 	protected double myGPA;      // grade point average
 	
+	// Parameterized constructor
 	public Student(String name, int age, String gender, String idNum, double gpa)  {
 		// use the super class’ constructor
 		super(name, age, gender);
@@ -14,6 +16,7 @@ public class Student extends Person{
 		myGPA = gpa;
 	}
 
+	// Getters
 	public String getIdNum(){
 		return this.myIdNum;
 	}
@@ -22,11 +25,17 @@ public class Student extends Person{
 		return this.myGPA;
 	}
 
+	// Setters
 	public void setIdNum(String idNum){
 		this.myIdNum = idNum;
 	}
 
 	public void setGPA(double gpa){
 		this.myGPA = gpa;
+	}
+
+	// Description
+	public String toString(){
+		return super.toString() + ", idNum: " + myIdNum + ", GPA: " + myGPA;
 	}
 }
