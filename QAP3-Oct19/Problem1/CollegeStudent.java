8c016e0088c39793e675f6f50bbb63efdaf38179
CameronBrenton <Cameron.Brenton@keyin.com>
2021-10-23 12:29:19 -0230

    Complete and comment the class

--- QAP3-Oct19/Problem1/CollegeStudent.java
+++ QAP3-Oct19/Problem1/CollegeStudent.java
@@ -2,6 +2,38 @@
 * This is the CollegeStudent subclass of Student.
 */
 
+
 public class CollegeStudent extends Student{
-	
+	protected String major; // College student's class major
+	protected int year; // College student's current enrollment year
+
+	// Class constructor
+	public CollegeStudent(String myName, int myAge, String myGender, String myIdNum, double myGPA, int year, String major){
+		super(myName, myAge, myGender, myIdNum, myGPA);
+		this.year = year;
+		this.major = major;
+	}
+
+	// Getters for the class variables
+	public int getYear(){
+		return year;
+	}
+
+	public String getMajor(){
+		return major;
+	}
+
+	// Setters for the class variables
+	public void setYear(int year){
+		this.year = year;
+	}
+
+	public void setMajor(String major){
+		this.major = major;
+	}
+
+	// Description
+	public toString(){
+		return super.toString() + ", Major: " + major + ", year: " + year;
+	}
 }
